4c90a15c02c8d31147367541679743076e6f9e99
yuanqingyeoh <yuanqingyeoh1999@gmail.com>
2024-12-22 15:43:07 +0800

    Add validation for trade other than supported trading pair

--- src/main/java/com/example/trading/trade/service/TradeService.java
+++ src/main/java/com/example/trading/trade/service/TradeService.java
@@ -69,6 +69,8 @@ public class TradeService {
         String pair1 = makeTradeDTO.getSymbol().substring(0, 3).toUpperCase();
         String pair2 = makeTradeDTO.getSymbol().substring(3).toUpperCase();
 
+        validateTradePair(pair1, pair2);
+
         Set<Balance> balances = userOpt.get().getBalances();
 
         CryptoPrice price = cryptoPriceService.getSymbol(makeTradeDTO.getSymbol());
@@ -80,80 +82,84 @@ public class TradeService {
 
         if (makeTradeDTO.getAction().equalsIgnoreCase(ACTION_BUY)) {
 
-            BigDecimal total = price.getAskPrice().multiply(makeTradeDTO.getQuantity());
-
             if (pair2BalanceOpt.isEmpty()){
                 throw new TradeException("Insufficient " + pair2 + " Balance");
             }
 
-            Balance pair2Balance = pair2BalanceOpt.get();
-            Balance pair1Balance = new Balance();
-            if (pair2Balance.getBalance().compareTo(total) >= 0) {
-                // able to buy
-                pair2Balance.setBalance(pair2Balance.getBalance().subtract(total));
+            BigDecimal total = price.getAskPrice().multiply(makeTradeDTO.getQuantity());
 
-
-                if (pair1BalanceOpt.isPresent()) {
-                    pair1Balance = pair1BalanceOpt.get();
-                    pair1Balance.setBalance(pair1BalanceOpt.get().getBalance().add(makeTradeDTO.getQuantity()));
-                } else {
-                    pair1Balance.setCoin(pair1);
-                    pair1Balance.setBalance(makeTradeDTO.getQuantity());
-                    pair1Balance.setUser(userOpt.get());
-                }
-
-                List<Balance> toSave = new ArrayList<>();
-                toSave.add(pair1Balance);
-                toSave.add(pair2Balance);
-                userService.saveBalances(toSave);
-
-                trade.setPrice(price.getAskPrice());
-
-                return tradeRepository.save(trade);
-
-
-            } else {
+            if (pair2BalanceOpt.get().getBalance().compareTo(total) < 0) {
                 // insufficient balance
                 throw new TradeException("Insufficient " + pair2 + " Balance");
             }
-        } else {
 
-            BigDecimal total = price.getBidPrice().multiply(makeTradeDTO.getQuantity());
+            Balance pair1Balance = new Balance();
+            Balance pair2Balance = pair2BalanceOpt.get();
+
+            pair2Balance.setBalance(pair2Balance.getBalance().subtract(total));
+
+            if (pair1BalanceOpt.isPresent()) {
+                pair1Balance = pair1BalanceOpt.get();
+                pair1Balance.setBalance(pair1BalanceOpt.get().getBalance().add(makeTradeDTO.getQuantity()));
+            } else {
+                pair1Balance.setCoin(pair1);
+                pair1Balance.setBalance(makeTradeDTO.getQuantity());
+                pair1Balance.setUser(userOpt.get());
+            }
+
+            List<Balance> toSave = new ArrayList<>();
+            toSave.add(pair1Balance);
+            toSave.add(pair2Balance);
+            userService.saveBalances(toSave);
+
+            trade.setPrice(price.getAskPrice());
+
+            return tradeRepository.save(trade);
+
+
+
+        } else {
 
             if (pair1BalanceOpt.isEmpty()){
                 throw new TradeException("Insufficient " + pair1 + " Balance");
             }
 
-            Balance pair1Balance = pair1BalanceOpt.get();
-            Balance pair2Balance = new Balance();
-            if (pair1Balance.getBalance().compareTo(makeTradeDTO.getQuantity()) >= 0) {
-                // able to sell
-                pair1Balance.setBalance(pair1Balance.getBalance().subtract(makeTradeDTO.getQuantity()));
+            BigDecimal total = price.getBidPrice().multiply(makeTradeDTO.getQuantity());
 
-                if (pair2BalanceOpt.isPresent()) {
-                    pair2Balance = pair2BalanceOpt.get();
-                    pair2Balance.setBalance(pair2BalanceOpt.get().getBalance().add(total));
-                } else {
-                    pair2Balance.setCoin(pair2);
-                    pair2Balance.setBalance(makeTradeDTO.getQuantity());
-                    pair2Balance.setUser(userOpt.get());
-                    balances.add(pair1Balance);
-                }
-
-                List<Balance> toSave = new ArrayList<>();
-                toSave.add(pair1Balance);
-                toSave.add(pair2Balance);
-                userService.saveBalances(toSave);
-
-
-                trade.setPrice(price.getBidPrice());
-
-                return tradeRepository.save(trade);
-
-            } else {
+            if (pair1BalanceOpt.get().getBalance().compareTo(makeTradeDTO.getQuantity()) < 0) {
                 // insufficient balance
                 throw new TradeException("Insufficient " + pair1 + " Balance");
             }
+
+            Balance pair1Balance = pair1BalanceOpt.get();
+            Balance pair2Balance = new Balance();
+
+            pair1Balance.setBalance(pair1Balance.getBalance().subtract(makeTradeDTO.getQuantity()));
+
+            if (pair2BalanceOpt.isPresent()) {
+                pair2Balance = pair2BalanceOpt.get();
+                pair2Balance.setBalance(pair2BalanceOpt.get().getBalance().add(total));
+            } else {
+                pair2Balance.setCoin(pair2);
+                pair2Balance.setBalance(makeTradeDTO.getQuantity());
+                pair2Balance.setUser(userOpt.get());
+            }
+
+            List<Balance> toSave = new ArrayList<>();
+            toSave.add(pair1Balance);
+            toSave.add(pair2Balance);
+            userService.saveBalances(toSave);
+
+            trade.setPrice(price.getBidPrice());
+
+            return tradeRepository.save(trade);
+
+        }
+    }
+
+    private void validateTradePair(String pair1, String pair2) throws TradeException {
+        if (!"BTC".equalsIgnoreCase(pair1) || !"ETH".equalsIgnoreCase(pair1) || !"USDT".equalsIgnoreCase(pair2)) {
+            throw new TradeException("Only BTCUSDT and ETHUSDT trade pair supported now.");
         }
     }
 
